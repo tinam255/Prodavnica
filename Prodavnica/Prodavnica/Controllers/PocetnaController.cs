52fa2e0c4fb0309454e158c8611ff96a29cf5990
Tina Mladenovic <tmladenovic99@gmail.com>
2024-06-01 16:34:58 +0200

    Added json file

--- Prodavnica/Prodavnica/Controllers/PocetnaController.cs
+++ Prodavnica/Prodavnica/Controllers/PocetnaController.cs
@@ -2,6 +2,10 @@
 using System.Linq;
 using System.Web.Mvc;
 using Prodavnica.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Prodavnica.Controllers
 {
@@ -13,7 +17,18 @@ namespace Prodavnica.Controllers
         public ActionResult Index()
         {
             var proizvodis = db.proizvodis.Include(p => p.dobavljaci).Include(p => p.kategorije).Include(p => p.proizvodjaci);
-            return View(proizvodis.ToList());
+
+            string proizvodi_json_putanja = Server.MapPath(@"Proizvodi JSON\proizvodi.json");
+
+            StreamReader sr = new StreamReader(proizvodi_json_putanja);
+
+            string json = sr.ReadToEnd();
+
+            List<proizvodi> proizvodi_json_kontent = new List<proizvodi>();
+
+            proizvodi_json_kontent = JsonConvert.DeserializeObject<List<proizvodi>>(json);
+
+            return View(proizvodis.ToList().Concat(proizvodi_json_kontent));
         }
     }
 }
